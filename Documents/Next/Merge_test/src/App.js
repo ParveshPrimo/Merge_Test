d4b8c14a817c93f847e8a68218358908da7bc6e7
Parvesh <parveshk.primotech@gmail.com>
2025-01-15 19:05:24 +0530

    Initial commit: Basic Todo App implementation

---
+++ Documents/Next/Merge_test/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import TodoForm from './components/TodoForm';
+import TodoItem from './components/TodoItem';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Math.random().toString(),
+      text,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <TodoForm addTodo={addTodo} />
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App; 
